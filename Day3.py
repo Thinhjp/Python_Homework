b36cd94660c775a4eb7c685a337f157d4b8ad105
TIM TRINH <thinhjp.acedamy@gmail.com>
2026-04-24 23:24:36 +0900

    Update_Day3_24-4

--- Day3.py
+++ Day3.py
@@ -37,35 +37,35 @@
     
 # 03. Gợi ý sản phẩm liên quan [LIST]
 
-products = [
-{"id": 1, "name": "Áo polo", "category": "ao", "rating": 4.5},
-{"id": 2, "name": "Áo thun", "category": "ao", "rating": 4.8},
-{"id": 3, "name": "Áo khoác", "category": "ao", "rating": 4.2},
-{"id": 4, "name": "Quần jeans","category": "quan","rating": 4.7},
-{"id": 5, "name": "Áo sơ mi", "category": "ao", "rating": 4.6},
-]
+# products = [
+# {"id": 1, "name": "Áo polo", "category": "ao", "rating": 4.5},
+# {"id": 2, "name": "Áo thun", "category": "ao", "rating": 4.8},
+# {"id": 3, "name": "Áo khoác", "category": "ao", "rating": 4.2},
+# {"id": 4, "name": "Quần jeans","category": "quan","rating": 4.7},
+# {"id": 5, "name": "Áo sơ mi", "category": "ao", "rating": 4.6},
+# ]
 
-def related_products(product_id, products, limit):
-    # tạo vòng lặp để lấy category từ id được truyền vào. VD: truyền id = 1 thì lấy "ao"
-    get_category = None
-    for pro in products:
-        if pro["id"] == product_id:
-            get_category = pro["category"]
-            break
-    if get_category == None: #nếu key truyền vào không xuất hiện thì end sớm về chuỗi rỗng
-        return []
-    # Gom các sản phẩm chung category nhưng khác id, để không bị lặp sản phẩm hiện tại đang input vào.
-    filtered_list = []
-    for pro in products:
-        if pro["category"] == get_category and pro["id"] != product_id:
-            filtered_list.append(pro)
-    def rate(item):
-        return item["rating"]
-    sorted_list = sorted(filtered_list, key = rate, reverse=True)
-    return sorted_list[:limit]
+# def related_products(product_id, products, limit):
+#     # tạo vòng lặp để lấy category từ id được truyền vào. VD: truyền id = 1 thì lấy "ao"
+#     get_category = None
+#     for pro in products:
+#         if pro["id"] == product_id:
+#             get_category = pro["category"]
+#             break
+#     if get_category == None: #nếu key truyền vào không xuất hiện thì end sớm về chuỗi rỗng
+#         return []
+#     # Gom các sản phẩm chung category nhưng khác id, để không bị lặp sản phẩm hiện tại đang input vào.
+#     filtered_list = []
+#     for pro in products:
+#         if pro["category"] == get_category and pro["id"] != product_id:
+#             filtered_list.append(pro)
+#     def rate(item):
+#         return item["rating"]
+#     sorted_list = sorted(filtered_list, key = rate, reverse=True)
+#     return sorted_list[:limit]
 
-test = related_products(product_id=1, products=products, limit=3)
-print(test)
+# test = related_products(product_id=1, products=products, limit=3)
+# print(test)
             
 #04. Phát hiện đơn hàng bất thường [LIST]
 orders = [
@@ -78,8 +78,8 @@ orders = [
 def detect_anomalies(orders, threshold=2.5):
     if orders == None:
         return[]
-    for order in orders:
-        total = sum(order["total"])
+    
+    total = sum(order["total"] for order in orders)
     # Trung bình
     avg = total / len(orders)
     #Tạo giới hạn
