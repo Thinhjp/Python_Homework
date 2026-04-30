ed1c923ebdc0245b06d80431bc6ad0d7656a9961
s425528 <s425528@ad.shumei-u.ac.jp>
2026-04-30 14:30:27 +0900

    Update_30.4Ver1

--- Day4.py
+++ Day4.py
@@ -1,5 +1,26 @@
+class BrowserHistory:
+    def __init__(self, homepage: str):
+        self.history = [homepage]
+        self.current_index = 0
+
+    def visit(self, url: str) -> None: 
+        # -> None nghĩa là hàm này không trả về giá trị nào, 
+        # nó chỉ thực hiện hành động cập nhật lịch sử trình duyệt
+        self.history = self.history[:self.current_index + 1]
+        # 
+        self.history.append(url)
+        self.current_index += 1
+
+    def back(self, steps: int) -> str:
+        self.current_index = max(0, self.current_index - steps)
+        return self.history[self.current_index]
+
+    def forward(self, steps: int) -> str:
+        self.current_index = min(len(self.history) - 1, self.current_index + steps)
+        return self.history[self.current_index]
 h = BrowserHistory("trang-chu")
 h.visit("san-pham/ao-thun")
+h.back(1)
 h.visit("san-pham/quan-jean")
 h.visit("gio-hang")
 
